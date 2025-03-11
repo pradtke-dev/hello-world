630e3a6d9e452604d479920b713fd7a40aa906bb
Bastian Schmack <bastian.schmack@pradtke.de>
2025-03-11 16:49:51 +0100

    Add unit tests for TodoModel and include test project in solution

---
+++ Hello.Tests/Pages/TodoModelTest.cs
@@ -0,0 +1,42 @@
+﻿using Hello.Pages;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Hello.Tests.Pages;
+
+[TestClass]
+public class TodoModelTests
+{
+    public TodoModel TodoModel { get; set; }
+    
+    [TestInitialize]
+    public void Init()
+    {
+        var loggerMock = new Mock<ILogger<TodoModel>>();
+        TodoModel = new TodoModel(loggerMock.Object);
+    }
+    
+    [TestMethod]
+    public void OnPost_EmptyTodo_DoesNotAddTodo()
+    {
+        // Simulate empty input
+        var initialCount = TodoModel.Todos.Count;
+        
+        // Act - No addition to the list since it's empty input
+
+        // Assert
+        Assert.AreEqual(initialCount, TodoModel.Todos.Count);
+    }
+    
+    [TestMethod]
+    public void OnPost_ValidTodo_AddsTodoToList()
+    {
+        // Simulate setting the todo directly
+        TodoModel.Todos.Add("Test Todo");
+
+        // Assert
+        Assert.AreEqual(1, TodoModel.Todos.Count);
+        Assert.AreEqual("Test Todo", TodoModel.Todos[0]);
+    }
+}
